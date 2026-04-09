7c211e064f26f28dcc05b5b17fcf5990efa3555e
Lavínia <lavizsenai@gmail.com>
2026-04-09 11:09:44 -0300

    tentando conectar o banco aaa

---
+++ src/main/resources/db.migration/V1__create-table-clientes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE clientes(
+     id bigint not null auto_increment,
+     nome varchar(100) not null,
+     email varchar(255) not null unique,
+     cpf varchar(11) not null unique,
+     telefone varchar(20),
+     ativo tinyint not null,
+
+     primary key(id)
+)
