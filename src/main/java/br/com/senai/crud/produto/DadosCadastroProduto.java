11e94e3e626942f2db7449800fdcd53c9a02fc0b
Lavínia <lavizsenai@gmail.com>
2026-04-27 11:00:18 -0300

    finalizado

---
+++ src/main/java/br/com/senai/crud/produto/DadosCadastroProduto.java
@@ -0,0 +1,32 @@
+package br.com.senai.crud.produto;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+public record DadosCadastroProduto(
+        @NotBlank
+        @Size(min = 3, max = 100)
+        String nome,
+
+        @NotNull
+        @Positive
+        @Digits(integer = 10, fraction = 2)
+        BigDecimal preco,
+
+        @NotBlank
+        @Pattern(regexp = "^\\S{1,20}$", message = "SKU não pode conter espaços em branco")
+        @Size(max = 20)
+        String sku,
+
+        @Size(max = 255)
+        String descricao,
+
+        @NotNull
+        @PositiveOrZero
+        Long estoque,
+
+        @NotNull
+        Long categoriaId
+) {
+}
